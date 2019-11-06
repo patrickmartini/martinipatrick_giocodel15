37914633f53ff641b51c64c13c3c1fd24c8d5ccd
Patrick Martini <patrick.martini@barsanti.edu.it>
2019-11-06 12:51:01 +0100

    2 lezione
    Cominciato a scrivere il codice

--- mp-GiocoDel15/mp-GiocoDel15/ViewController.swift
+++ mp-GiocoDel15/mp-GiocoDel15/ViewController.swift
@@ -2,40 +2,82 @@
 //  ViewController.swift
 //  mp-GiocoDel15
 //
-//  Created by PATRICK MARTINI on 23/10/2019.
+//  Created by  PATRICK MARTINI on 23/10/2019.
 //  Copyright © 2019 PATRICK MARTINI. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btn7: UIButton!
+    @IBOutlet weak var btn8: UIButton!
+    @IBOutlet weak var btn9: UIButton!
+    @IBOutlet weak var btn10: UIButton!
+    @IBOutlet weak var btn11: UIButton!
+    @IBOutlet weak var btn12: UIButton!
+    @IBOutlet weak var btn13: UIButton!
+    @IBOutlet weak var btn14: UIButton!
+    @IBOutlet weak var btn15: UIButton!
+    @IBOutlet weak var btnSposto: UIButton!
+    
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    //var arr : [Int] = []
-   // let arrNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-        
+        var arr : [Int] = []
+        let arrNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
     }
     
-/*   func Gioco15()
+    func lock()
     {
-        var pos = 2
-        let help = 0
-        
-        let a = 1
-        
-        pos1 = a + pos + help
-        if pos1 == 2
-        {
-         print("")
-        }
-        else
-        {
-            print("sticchiu")
-        }
-        
-    }*/
+        btn1.isUserInteractionEnabled = false
+        btn2.isUserInteractionEnabled = false
+        btn3.isUserInteractionEnabled = false
+        btn4.isUserInteractionEnabled = false
+        btn5.isUserInteractionEnabled = false
+        btn6.isUserInteractionEnabled = false
+        btn7.isUserInteractionEnabled = false
+        btn8.isUserInteractionEnabled = false
+        btn9.isUserInteractionEnabled = false
+        btn10.isUserInteractionEnabled = false
+        btn11.isUserInteractionEnabled = false
+        btn12.isUserInteractionEnabled = false
+        btn13.isUserInteractionEnabled = false
+        btn14.isUserInteractionEnabled = false
+        btn15.isUserInteractionEnabled = false
     }
-
+    
+    func unlock()
+    {
+        btn1.isUserInteractionEnabled = true
+        btn2.isUserInteractionEnabled = true
+        btn3.isUserInteractionEnabled = true
+        btn4.isUserInteractionEnabled = true
+        btn5.isUserInteractionEnabled = true
+        btn6.isUserInteractionEnabled = true
+        btn7.isUserInteractionEnabled = true
+        btn8.isUserInteractionEnabled = true
+        btn9.isUserInteractionEnabled = true
+        btn10.isUserInteractionEnabled = true
+        btn11.isUserInteractionEnabled = true
+        btn12.isUserInteractionEnabled = true
+        btn13.isUserInteractionEnabled = true
+        btn14.isUserInteractionEnabled = true
+        btn15.isUserInteractionEnabled = true
+    }
+    
+    func Gioco15()
+    {
+        //Tasi Patrick
+    }
+    
 }
+
