e52b11bd524872b1b68c0d4751031b29df0d20fc
Patrick Martini <patrick.martini@barsanti.edu.it>
2019-11-06 11:09:55 +0100

    Inizio...

---
+++ mp-GiocoDel15/mp-GiocoDel15/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  mp-GiocoDel15
+//
+//  Created by PATRICK MARTINI on 23/10/2019.
+//  Copyright © 2019 PATRICK MARTINI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    //var arr : [Int] = []
+   // let arrNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+        
+    }
+    
+/*   func Gioco15()
+    {
+        var pos = 2
+        let help = 0
+        
+        let a = 1
+        
+        pos1 = a + pos + help
+        if pos1 == 2
+        {
+         print("")
+        }
+        else
+        {
+            print("sticchiu")
+        }
+        
+    }*/
+    }
+
+}
---
+++ mp-GiocoDel15/mp-GiocoDel15Tests/mp_GiocoDel15Tests.swift
@@ -0,0 +1,34 @@
+//
+//  mp_GiocoDel15Tests.swift
+//  mp-GiocoDel15Tests
+//
+//  Created by PATRICK MARTINI on 23/10/2019.
+//  Copyright © 2019 PATRICK MARTINI. All rights reserved.
+//
+
+import XCTest
+@testable import mp_GiocoDel15
+
+class mp_GiocoDel15Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
